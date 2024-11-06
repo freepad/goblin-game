86edae97107a7bd528896d2b5b74157bd75aab2d
Mikhail Ignatov <ignatov.m55@gmail.com>
2024-11-06 17:47:42 +0600

    version 22

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,26 +1,39 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/js/game.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    mode: 'development',
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    devServer: {
-        static: './dist',
-    },
+  entry: './src/js/game.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      // Обработка CSS файлов
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      // Обработка изображений
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+      // Обработка JavaScript с использованием Babel
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'], // Поддержка современных JavaScript фич
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+  },
 };
