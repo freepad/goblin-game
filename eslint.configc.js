adce4bb365e08873cee1d3c78cbd92a065518cf1
Mikhail Ignatov <ignatov.m55@gmail.com>
2024-11-06 16:32:08 +0600

    version 16

---
+++ eslint.configc.js
@@ -0,0 +1,22 @@
+const { defineConfig } = import('eslint-define-config');
+
+module.exports = defineConfig({
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off', // для React 17 и выше
+  },
+});
