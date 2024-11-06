8b0819033a29134ecb9e6e7f8cbdfd0b7bf25a16
Mikhail Ignatov <ignatov.m55@gmail.com>
2024-11-06 19:56:29 +0600

    version 27

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -4,7 +4,9 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   target: "web",
+  entry: "./src/js/game.js",  // Убедитесь, что файл index.js существует в папке src
   output: {
+    filename: "bundle.js",  // Выходной файл для вашего JS
     path: path.resolve(__dirname, "dist"),
     publicPath: "",
   },
@@ -37,7 +39,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
+      template: "./src/index.html",  // Убедитесь, что файл index.html существует в папке src
       filename: "./index.html",
     }),
     new MiniCssExtractPlugin({
